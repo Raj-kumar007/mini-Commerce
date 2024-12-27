c6f7b3796436003ded1572888a1d20526a7e9d41
@parjkumar184 <parjkumar184@gmail.com>
2024-12-27 18:55:24 +0530

    third commit

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,40 +4,40 @@ import Products from '../components/Products'
 import { useSearchParams } from 'react-router';
 
 const Home = () => {
-    
-const [products, setproducts]= useState([]);
 
- const [searchParam] = useSearchParams();
-useEffect(() => {
-  const fetchProducts = async () => {
-    try {
-      const response = await fetch('http://localhost:8000/api/v1/products?'+searchParam);
-      const data = await response.json();
-      setproducts(data.products);
-      
-    } catch (err) {
-      console.error('Error fetching products:', err);
-    }
-  };
+  const [products, setproducts] = useState([]);
 
-  fetchProducts();
-}, [searchParam]); 
+  const [searchParam] = useSearchParams();
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch('http://localhost:8000/api/v1/products?' + searchParam);
+        const data = await response.json();
+        setproducts(data.products);
 
-  return (    
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    fetchProducts();
+  }, [searchParam]);
+
+  return (
+
+
     <Fragment>
- <h1 id="products_heading">Latest Products</h1>
+      <h1 id="products_heading">Latest Products</h1>
 
-<section id="products" className="container mt-5">
-  <div className="row">
+      <section id="products" className="container mt-5">
+        <div className="row">
 
-    {products.length > 0 ? products.map(product => <Products product={product}/>
-    ) :
-    <h2>No products found</h2>}
-    
-   
-  </div>
-</section> 
+          {products.length > 0 ? products.map(product => <Products product={product} />
+          ) :
+            <h2>No products found</h2>}
 
+
+        </div>
+      </section>
     </Fragment>
   )
 }
